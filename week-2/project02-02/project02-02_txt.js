32504a909cfc79a9c84f69d244e44cdd733b099f
Sara King <sara.king@kohls.com>
2025-01-16 11:07:51 -0700

    02-02

--- week-2/project02-02/project02-02_txt.js
+++ week-2/project02-02/project02-02_txt.js
@@ -3,9 +3,22 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Sara King
+      Date: 01/16/2025
 
       Filename: project02-02.js
  */
- 
+
+function verifyForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const phone = document.getElementById("phone").value;
+
+  if (name && email && phone) {
+    window.alert("Thank you!");
+  } else {
+    window.alert("Please fill in all fields");
+  }
+}
+
+document.getElementById("submit").addEventListener("click", verifyForm);
